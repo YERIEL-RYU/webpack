a28f6909031a966ba3f00f7b9f795a3d107b203e
YERIEL-RYU <yeriel.ryu@gmail.com>
2020-09-14 16:36:15 +0900

    webpack 빌드 디렉토리 깨끗하게 하기 clean-webpack-plugin 사용

--- webpackEx/webpack.config.js
+++ webpackEx/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -47,6 +48,7 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename:'style.css'
-    })
+    }),
+    new CleanWebpackPlugin()
   ]
 };
